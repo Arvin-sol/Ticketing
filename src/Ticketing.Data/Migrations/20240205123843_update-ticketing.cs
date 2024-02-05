d19e0a8a6795a328149215a11496307aa985b52d
Lenovo <Lenovo@DESKTOP-588QI00>
2024-02-05 16:09:07 +0330

    new migration

---
+++ src/Ticketing.Data/Migrations/20240205123843_update-ticketing.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketing.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateticketing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Status",
+                schema: "base",
+                table: "TicketMesseges",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                schema: "base",
+                table: "TicketMesseges");
+        }
+    }
+}
